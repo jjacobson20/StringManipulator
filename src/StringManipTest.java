ae34714b5c3bfa428be12d593733f85cd1929864
jjacobson20 <jjacobson20@HS-MATH2-09.gvsd.org>
2018-10-16 13:36:43 -0400

    Scanner

--- src/StringManipTest.java
+++ src/StringManipTest.java
@@ -1,9 +1,19 @@
+/**
+ * 
+ * @author jeremy
+ *
+ */
+import java.util.Scanner;
 
 public class StringManipTest
 {
 	public static void main(String[] args)
 	{
 		StringManipulator stringManip = new StringManipulator();
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("What method would you like to run?");
+		
 		
 		System.out.println(stringManip.reverse("cool text"));
 		System.out.println(stringManip.noVowels("test these vowels"));
